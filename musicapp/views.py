c6b18ec15a69d17a36c85f60b441568526a6bb30
Zainab omolola fadeyi <zainabfadeyi@gmail.com>
2022-11-03 20:02:06 +0530

    simple REST API

--- musicapp/views.py
+++ musicapp/views.py
@@ -5,3 +5,24 @@ from django.http import HttpResponse
 # Create your views here.
 def index (request):
     return HttpResponse('yahhh')
+
+from rest_framework import generics
+
+from .models import Song, Artiste
+from .serializers import SongSerializer, ArtisteSerializer
+
+class SongList(generics.ListCreateAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+class SongDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+class ArtisteList(generics.ListCreateAPIView):
+    queryset = Artiste.objects.all()
+    serializer_class = ArtisteSerializer
+
+class ArtisteDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Artiste.objects.all()
+    serializer_class = ArtisteSerializer
